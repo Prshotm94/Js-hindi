6a01469cbc735f080862f96445f87a0b4a5e710e
Prshotm94 <prshotm7694@gmail.com>
2024-10-16 12:19:05 +0000

    varibles

---
+++ 01_basics/variables.js
@@ -0,0 +1,5 @@
+let username="prshotm";// used
+var rollno=1;//don't use because of scope problem
+const account_id =22;//can't change the value
+address="jammu";//don't use 
+console.table([username, rollno, account_id, address]);//print in tabular form
